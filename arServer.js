19e76d0d6f124643b330374d51f844d194111e80
Mike <titanix@list.ru>
2018-04-04 21:20:38 +0300

    First Commit!

---
+++ arServer.js
@@ -0,0 +1,19 @@
+
+var express = require('express');
+var app = express();
+var request = require('request');
+var bodyParser = require ('body-parser');
+
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+    request('https://api.guildwars2.com/v2/commerce/prices/49424', function (error, response, body){
+        var r = body.id;
+        res.json (r);     
+      })
+    });
+
+app.listen(3000, function () {
+  console.log('App is running');
+});
+
