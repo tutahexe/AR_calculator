34894fccf576652ada45d7db8bf7980140c2a043
Mike <titanix@list.ru>
2018-04-04 22:06:46 +0300

    Updated arServer to the worker version!

--- arServer.js
+++ arServer.js
@@ -7,11 +7,14 @@ var bodyParser = require ('body-parser');
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-    request('https://api.guildwars2.com/v2/commerce/prices/49424', function (error, response, body){
+    request({
+      url: 'https://api.guildwars2.com/v2/commerce/prices/49424',
+      json: true
+      }, function (error, response, body){
         var r = body.id;
-        res.json (r);     
-      })
-    });
+        res.json(r);     
+    })
+});
 
 app.listen(3000, function () {
   console.log('App is running');
