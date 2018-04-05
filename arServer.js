ab8ca816e7fa240481ad2ff8488e2f36359914d2
unknown <mdovhyi@mdovhyi.exigengroup.com>
2018-04-05 13:07:53 +0300

    Added hello basic pug template

--- arServer.js
+++ arServer.js
@@ -4,17 +4,20 @@ var app = express();
 var request = require('request');
 var bodyParser = require ('body-parser');
 
+app.set("view engine", "pug");
+
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-    request({
-      url: 'https://api.guildwars2.com/v2/commerce/prices/49424',
-      json: true
-      }, function (error, response, body){
-        var r = body.id;
-        res.json(r);     
-    })
-});
+    // request({
+    //   url: 'https://api.guildwars2.com/v2/commerce/prices/49424',
+    //   json: true
+    //   }, function (error, response, body){
+    //     var r = body.id;
+    //     res.json(r);     
+    // })
+res.render ("index");
+  });
 
 app.listen(3000, function () {
   console.log('App is running');
